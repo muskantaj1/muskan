333a5442335a9117fda82ae71d00b425e57c75a4
muskantaj1 <35479000+muskantaj1@users.noreply.github.com>
2018-02-06 09:17:04 +0530

    Add files via upload

---
+++ com_pro.py
@@ -0,0 +1,12 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> x=complex(1,2)
+>>> type(x)
+<class 'complex'>
+>>> print(x)
+(1+2j)
+>>> z=1+2j
+>>> type(z)
+<class 'complex'>
+>>> 
---
+++ ex.py
@@ -0,0 +1,6 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print('print("print")')
+print("print")
+>>> 
