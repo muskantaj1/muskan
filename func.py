75ecc36575ca022849131bde991f666432c3e045
muskantaj1 <35479000+muskantaj1@users.noreply.github.com>
2018-02-24 10:13:40 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+print(add(7,3))
+print(sub(7,3))
+print(mul(7,3))
+print(div(7,3))
