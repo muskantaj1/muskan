2f623906b2e40837ceb90fb72f880be63ce72d89
muskantaj1 <35479000+muskantaj1@users.noreply.github.com>
2018-03-13 10:18:51 +0530

    Add files via upload

---
+++ graph1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 09:52:08 2018
+
+@author: cs2017a126
+"""
+
+import pylab
+a=[5,10,15,20,25]
+b=[0.15,0.42,0.47,0.59,0.68]
+pylab.figure("pKa_pH")
+pylab.plot(a,b)
+pylab.show()
